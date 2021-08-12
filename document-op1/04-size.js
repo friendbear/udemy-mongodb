57b1dc23eb3ba1123a0132442894d8fa1d5959a1
T Kumagai <kumagai@bear.plala.or.jp>
2021-08-12 11:28:17 +0900

    $size演算子

--- document-op1/04-size.js
+++ document-op1/04-size.js
@@ -5,3 +5,9 @@ db.books.find({
         $gt: [{ size: "categories" }, 1]  // sizeに$ を付けるとエラーになる
     }
 }).pretty()
+
+db.books.find({
+    $expr: {
+        $gt: [{ $size: "$categories" }, 1]  // $size演算子は、$size: "$categories" のように$を付ける必要がある
+    }
+}).pretty()
